53b59676c8fe1b49aa3049fd0310fa5d1a699b09
corinbeauty <pq0800@gmail.com>
2026-08-11 15:30:58 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,20 @@
 window.addEventListener("message", (event) => {
     if (event.source !== window) return;
-    if (event.data.type && event.data.type === "REQUEST_CAPTURE") {
+    
+    // 截圖指令
+    if (event.data.type === "REQUEST_CAPTURE") {
         chrome.runtime.sendMessage({ action: "CAPTURE_SCREEN" }, (response) => {
             if (response && response.dataUrl) {
                 window.postMessage({ type: "RECEIVE_CAPTURE", dataUrl: response.dataUrl }, "*");
             }
         });
+    }
+    // 放大指令
+    else if (event.data.type === "REQUEST_MAXIMIZE") {
+        chrome.runtime.sendMessage({ action: "MAXIMIZE_WINDOW" });
+    }
+    // 縮小指令
+    else if (event.data.type === "REQUEST_MINIMIZE") {
+        chrome.runtime.sendMessage({ action: "MINIMIZE_WINDOW" });
     }
 });
